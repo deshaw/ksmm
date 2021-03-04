27ef62f75c11b388aa04055419fc66e53ebb5b3c
anirrudh <anik597@gmail.com>
2021-03-03 21:20:55 -0500

    initialize frontend

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { MainAreaWidget } from '@jupyterlab/apputils';
+
+import { ILauncher } from '@jupyterlab/launcher';
+
+import { CounterWidget } from './widget';
+/*
+ * The command IDs used by the react-widget plugin.
+ */
+namespace CommandIDs {
+  export const create = 'create-conda-store-widget';
+}
+
+/**
+ * Initialization data for the react-widget extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'conda-store-widget',
+  autoStart: true,
+  optional: [ILauncher],
+  activate: (app: JupyterFrontEnd, launcher: ILauncher) => {
+    const { commands } = app;
+
+    const command = CommandIDs.create;
+    commands.addCommand(command, {
+      caption: 'A way to manage IPython Kernels',
+      label: 'ksmm',
+      icon: args => (args['isPalette'] ? null : null),
+      execute: () => {
+	const content = new CounterWidget();
+        const widget = new MainAreaWidget<CounterWidget>({ content });
+        widget.title.label = 'ksmm';
+        app.shell.add(widget, 'main');
+      }
+    });
+
+    if (launcher) {
+      launcher.add({
+        command
+      });
+    }
+  }
+};
+
+export default extension;
