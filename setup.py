364bc25bd9672b4eabe44c1b54d081015f59266c
anirrudh <anik597@gmail.com>
2021-03-03 22:58:59 -0500

    almost working - checking in

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import pathLib
+import pathlib
 import json
 import setuptools
 import jupyter_packaging
 
 VERSION = '0.0.1'
-HERE =  Path(__file__).parent.resolve()
+HERE =  pathlib.Path(__file__).parent.resolve()
 # Name of the project
 name = "ksmm"
 lab_path = HERE / name / "labextension"
@@ -17,17 +17,26 @@ package_data_spec = {name: ["*"]}
 
 labext_name = "@jupyterlab-ipython/KernelSpec-Manager"
 
+
+data_files_spec = [
+    ("share/jupyter/labextensions/%s" % labext_name, str(lab_path), "**"),
+    ("share/jupyter/labextensions/%s" % labext_name, str(HERE), "install.json"),
+]
+
 cmdclass = jupyter_packaging.create_cmdclass(
-        "jsdeps", package_data_spec=package_data_spec, data_files_spec=data_files_spec 
-        )
+    "jsdeps", package_data_spec=package_data_spec, data_files_spec=data_files_spec
+)
 
 js_command = jupyter_packaging.combine_commands(
         jupyter_packaging.install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
         jupyter_packaging.ensure_targets(jstargets),
         )
 
-pkg_json = json.loads(HERE / "package.json").read_bytes())
-
+is_repo = (HERE / ".git").exists()
+if is_repo:
+    cmdclass["jsdeps"] = js_command
+else:
+    cmdclass["jsdeps"] = skip_if_exists(jstargets, js_command)
 
 setup_args = dict(
     name='ksmm',
