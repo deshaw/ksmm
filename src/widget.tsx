f81f6e022b0485ec45883783dd76ca03615c98dc
anirrudh <anik597@gmail.com>
2021-03-05 15:02:47 -0500

    update widget

--- src/widget.tsx
+++ src/widget.tsx
@@ -1,25 +1,48 @@
 import { ReactWidget } from '@jupyterlab/apputils';
-
-import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
+import { JSONSchema7 } from "json-schema";
+import Form from "@rjsf/core";
 
 /**
- * React component for a counter.
+ * React component for listing the possible
+ * ipykernel options.
  *
  * @returns The React component
  */
 const CounterComponent = (): JSX.Element => {
-  const [counter, setCounter] = useState(0);
+  const [data, setData] = useState(null);
+  const [showForm, setShowForm] = useState(false);
+  
+  const ipyschema: JSONSchema7 = {"title": "ipykernel mm", "type": "object", "properties": {"argv": {"type": "array", "items": {"type": "string"}}, "env": {"type": "object"}, "display_name": {"type": "string"}, "language": {"type": "string"}, "interrupt_mode": {"type": "string"}, "metadata": {"type": "object"}}, "required": ["argv", "display_name", "env", "interrupt_mode", "language", "metadata"]};
+
+  useEffect(() => {
+
+  const url  = 'http://localhost:8888/ks';
+  const kernelSpec = async () => {
+	  const response = await fetch(url);
+	  const jsondata = response.json();
+	  console.log(jsondata);
+	  setData(jsondata);
+	  setShowForm(true);
+  };
+
+  const timer = setTimeout(() => {
+	  kernelSpec();
+  }, 3000);
+
+ 
+  return () => clearTimeout(timer);
+  }, [
+	 data
+  ]);
 
   return (
     <div>
-      <p>You clicked {counter} times!</p>
-      <button
-        onClick={(): void => {
-          setCounter(counter + 1);
-        }}
-      >
-        Increment
-      </button>
+	    {showForm ?
+		    <Form schema={ipyschema} /> :
+		    <p> Loading... </p>
+	    }
     </div>
   );
 };
