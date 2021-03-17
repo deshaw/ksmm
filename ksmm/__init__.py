c22f9b6cdedf70b2d0fc599c6612c7687e38a2f7
anirrudh <anik597@gmail.com>
2021-03-17 10:55:02 -0400

    checking in

--- ksmm/__init__.py
+++ ksmm/__init__.py
@@ -6,7 +6,7 @@ from jupyter_server.serverapp import ServerApp
 from jupyter_server.utils import url_path_join as ujoin, url2path
 
 
-from .handlers import KSHandler
+from .handlers import setup_handlers
 
 
 HERE = pathlib.Path(__file__).parent.resolve()
@@ -29,20 +29,11 @@ def _jupyter_server_extension_paths():
 def _jupyter_server_extension_points():
     return [{"module": "ksmm"}]
 
-def _load_jupyter_server_extension(server_app: ServerApp(
-            default_url = 'ks',
-            base_url = 'ks',
-            disable_check_xsrf = True, 
-            )):
-    print(dir(server_app))
-    handlers = [
-            ("/(\w+)", KSHandler)
-            ]
-    k_handlers = [(ujoin(server_app.base_url, x[0]), x[1]) for x in handlers]
-    print(k_handlers)
+def _load_jupyter_server_extension(server_app):
 
-    server_app.web_app.add_handlers(".*", k_handlers)
+    url_path = "ks"
 
+    setup_handlers(server_app.web_app, server_app.kernel_spec_manager_class(), url_path)
 
     server_app.launch_instance
     
