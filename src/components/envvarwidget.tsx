2a5c49c8f70f30a47838e822d6016f96b7a04d7a
anirrudh <anik597@gmail.com>
2021-05-31 18:33:23 -0400

    checking in

---
+++ src/components/envvarwidget.tsx
@@ -0,0 +1,14 @@
+//import React, {useState} from 'react';
+
+// export const EnvVariablesField = (props: any) => {
+// 	return (
+//     <div id="custom">
+//       <p>Yeah, I'm pretty dumb.</p>
+//       <div>My props are: {JSON.stringify(props)}</div>
+//     </div>
+//   );
+// };
+
+// export const envvarsfield = {
+//   SchemaField: EnvVariablesField
+// };
--- src/components/ipythonformgroup.tsx
+++ src/components/ipythonformgroup.tsx
@@ -11,21 +11,51 @@ import React from "react";
  * 	 data -> The data to render - namely, the data as defined in the schema
  */
 export const IPythonFormGroup = (props: {
-  data: any;
+  properties: any;
   selecteditem: string;
+  mainprops: any;
+  handleAdditionalProperties: any; 
 }) => {
-  const fg: any = generateFormGroupMap(props.data);
+  const fg: any = generateFormGroupMap(props.properties);
   return (
     <div>
       {fg[props.selecteditem].map((index: number) => (
         <div className="property-wrapper" key={index}>
-          {props.data[index].content}
+          {props.properties[index].content}
         </div>
       ))}
     </div>
   );
 };
 
+/*
+ * This function is an ObjectFieldTemplate
+ * that is rendered for the environment variable.
+ */
+export const EnvVarForm = (props: any) => {
+  console.log(props);
+  return(
+    <div>
+        <input type="string" onChange={(e: any) => {console.log(e)}} />
+        <input type="string" onChange={(e: any) => {console.log(e)}} />
+        <button>Add New</button>
+      </div>
+  )
+}
+/*
+ * Return the Environment Schema to be able to 
+ * control the behavior of the onAddClick function,
+ * which will allow me to add more environment variables.
+ */
+// function getEnvironmentSchema(data: any) {
+//   const envSchema: Array<number> = [];  
+//   data.forEach((element: any) => {
+//     if (element.name == "env") {
+//       envSchema.push(data.indexOf(element))
+//      }
+// })
+//   return data[envSchema.pop()].content.props.schema;
+// }
 /*
  * Grab the location of the element in the array,
  * returning a value of the postions in the array.
@@ -36,7 +66,6 @@ function generateFormGroupMap(dataArr: any) {
   const EnvironmentVariableArray: Array<number> = [];
   const ComputeParametersArray: Array<number> = [];
   const MetadataArray: Array<number> = [];
-  console.log(dataArr);
   dataArr.forEach((element: any) => {
     if (
       element.name == "display_name" ||
