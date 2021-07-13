0f4c0d1be9252a8b640a6c331b16301b9b5d1b19
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-07-13 18:00:09 +0200

    remove print statement

--- ksmm/handlers.py
+++ ksmm/handlers.py
@@ -56,7 +56,6 @@ class KSCopyHandler(APIHandler):
    
     @tornado.web.authenticated
     def post(self):
-        print('copy handler called')
         data = tornado.escape.json_decode(self.request.body)
         source_dir = self.km.find_kernel_specs()[data["name"]]
         new_name = '-'.join([data["name"], "copy"])
